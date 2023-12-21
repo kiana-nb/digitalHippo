ceabced2e2fcfa2caa7c305051b7578fa089858a
Kiana Nb <kiana.nb34@gmail.com>
2023-12-22 00:11:23 +0330

    final final

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -3,8 +3,8 @@ import Link from "next/link";
 
 const ThankYouPage = async () => {
   return (
-    <main className="relative lg:min-h-full">
-      <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-screen lg:w-1/2 lg:pr-4 xl:pr-12">
+    <main className="relative h-screen">
+      <div className="hidden lg:block h-80 overflow-hidden lg:absolute lg:h-full lg:w-1/2 lg:pr-4 xl:pr-12">
         <Image
           fill
           src="/checkout-thank-you.jpg"
