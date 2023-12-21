08349ea50bc5d7e3ad984547afb9905463d10ff8
Kiana Nb <kiana.nb34@gmail.com>
2023-12-21 23:33:07 +0330

    final changes

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,26 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
 
 export function formatPrice(
   price: number | string,
   options: {
-    currency?: "USD" | "EUR" | "GBP" | "BDT"
-    notation?: Intl.NumberFormatOptions["notation"]
+    currency?: "USD" | "EUR" | "GBP" | "BDT";
+    notation?: Intl.NumberFormatOptions["notation"];
   } = {}
 ) {
-  const {currency = "USD", notation = "compact"} = options
+  const { currency = "USD", notation = "compact" } = options;
 
-  const numericPrice = typeof price === "string" ? parseFloat(price) : price
+  const numericPrice = typeof price === "string" ? parseFloat(price) : price;
 
-  return new Intl.NumberFormat("en-US",{
-    style : "currency",
+  return new Intl.NumberFormat("en-US", {
+    style: "currency",
     currency,
     notation,
-    maximumFractionDigits: 2
-  }).format(numericPrice)
+    maximumFractionDigits: 2,
+  }).format(numericPrice);
 }
